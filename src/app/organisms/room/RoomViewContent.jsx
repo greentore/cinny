7dd3b8b1238f577bcd16ceaed617388c9f1d7108
greentore <greentore@protonmail.com>
2023-04-17 05:27:14 +0200

    Dismiss pending notifications on focus

--- src/app/organisms/room/RoomViewContent.jsx
+++ src/app/organisms/room/RoomViewContent.jsx
@@ -483,6 +483,21 @@ function RoomViewContent({ eventId, roomTimeline }) {
     }
   }, [newEvent]);
 
+  // Dismiss pending notifications on focus
+  const onFocus = useCallback(() => {
+    const timelineScroll = timelineScrollRef.current;
+    if (timelineScroll.bottom < 16) {
+      requestAnimationFrame(() => markAsRead(roomTimeline.roomId))
+    }
+  }, [roomTimeline])
+
+  useEffect(() => {
+    window.addEventListener('focus', onFocus);
+    return () => {
+      window.removeEventListener('focus', onFocus);
+    };
+  }, [onFocus]);
+
   const listenKeyboard = useCallback((event) => {
     if (event.ctrlKey || event.altKey || event.metaKey) return;
     if (event.key !== 'ArrowUp') return;
